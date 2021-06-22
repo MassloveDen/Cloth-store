c7ce03d0146ccbda17ec796e6fd5b21f3454c252
MassloveDen <Denis132141@yandex.ru>
2021-06-22 15:23:21 +0300

    new bd and encoding with utf-8

--- main_store/settings.py
+++ main_store/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'main_store',
     'products',
+    'userss',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+AUTH_USER_MODEL = 'userss.User'
