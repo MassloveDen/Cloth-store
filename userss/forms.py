1e1317deb9e1ba1bdb0ea028b4c84228a66d530e
MassloveDen <Denis132141@yandex.ru>
2021-06-24 13:18:14 +0300

    authorisation form with DRY

--- userss/forms.py
+++ userss/forms.py
@@ -1,8 +1,19 @@
+from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from userss.models import User
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+         'placeholder':'Enter username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+         'placeholder':'Enter pass'}))
 
     class Meta:
         model = User
         fields = {'username', 'password'}
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+
--- userss/views.py
+++ userss/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
+from userss.forms import UserLoginForm
 
 # Create your views here.
 
+
+
+
 def login(request):
-    return render(request, 'userss/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('home'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'userss/login.html', context)
 
 def reg(request):
     return render(request, 'userss/register.html')
